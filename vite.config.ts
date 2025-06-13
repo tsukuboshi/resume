66702cab241271c636afe3058d518df394c6dcf5
tsukuboshi <53089786+tsukuboshi@users.noreply.github.com>
2025-06-13 14:03:57 +0900

    SSG設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,16 @@
 import build from "@hono/vite-build/cloudflare-workers";
-import adapter from "@hono/vite-dev-server/cloudflare";
+import ssg from "@hono/vite-ssg";
 import tailwindcss from "@tailwindcss/vite";
 import honox from "honox/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [
-    honox({
-      devServer: { adapter },
-      client: { input: ["./app/style.css"] },
-    }),
-    tailwindcss(),
-    build(),
-  ],
+const entry = "./app/server.ts";
+
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/honox-resume/" : "",
+  plugins: [honox(), tailwindcss(), build(), ssg({ entry })],
   server: {
     port: 5173,
     strictPort: true,
   },
-});
+}));
